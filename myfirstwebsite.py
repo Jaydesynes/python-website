d93b4a361d3e8ee0bcdaf8bf3a3872b6fcfb2710
Joshua <joshuamirigazhi@gmail.com>
2021-05-24 13:07:27 +0100

    first commit

---
+++ myfirstwebsite.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def Jaydesynes():
+    return"<p>Welcome to Jaydesynes</p>"
+
+@app.route("/About us")
+def jaydesynes():
+    return "<b>JAYDESYNES</b> is a Nigerian fast rising graphics company"
+
+@app.route("/Contact us")
+def jaydesynes():
+    return "<b>JAYDESYNES</b> Contact us on Facebook, Instagram @Jaydesynes"
+
+@app.route("/Our Services")
+def jaydesynes():
+    return "<b>JAYDESYNES</b> offers quality designs,from business cardsto logos, and lots more"
+
+@app.route("/About us")
+def jaydesynes():
+    return "<b>JAYDESYNES</b> is a Nigerian fast rising graphics company"
